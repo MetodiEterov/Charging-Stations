4ef7a16c2d80fd6c276ef923fd8ed1e91356cc2d
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:42:34 +0300

    Update LoggerManager.cs

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -1,46 +1,27 @@
-﻿namespace LoggerService
-{
-    using Entities.Contracts;
-    using NLog;
+using Entities.Contracts;
+using NLog;
 
-    /// <summary>
-    /// LoggerManager class
-    /// </summary>
+namespace LoggerService
+{
     public class LoggerManager : ILoggerManager
     {
         private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
-        /// <summary>
-        /// LogDebug method
-        /// </summary>
-        /// <param name="message"></param>
         public void LogDebug(string message)
         {
             Logger.Debug(message);
         }
 
-        /// <summary>
-        /// LogError method
-        /// </summary>
-        /// <param name="message"></param>
         public void LogError(string message)
         {
             Logger.Error(message);
         }
 
-        /// <summary>
-        /// LogInfo method
-        /// </summary>
-        /// <param name="message"></param>
         public void LogInfo(string message)
         {
             Logger.Info(message);
         }
 
-        /// <summary>
-        /// LogWarn method
-        /// </summary>
-        /// <param name="message"></param>
         public void LogWarn(string message)
         {
             Logger.Warn(message);
