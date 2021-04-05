982627304f971f8a0e16c89e78ae3530bbab9573
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:41:05 +0300

    Update ChargePointRequestModel.cs

--- Entities/Models/ChargePointRequestModel.cs
+++ Entities/Models/ChargePointRequestModel.cs
@@ -1,23 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.Models
 {
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
-    /// <summary>
-    /// ChargePointRequestModel class
-    /// </summary>
     public class ChargePointRequestModel
     {
-        /// <summary>
-        /// LocationId property
-        /// </summary>
         [Required]
         [StringLength(39)]
         public string LocationId { get; set; }
 
-        /// <summary>
-        /// ChargePoints property
-        /// </summary>
         public ICollection<ChargePoint> ChargePoints { get; set; }
     }
 }
