238e85be198caf6f49002834ee9efce7857e20e3
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:36:22 +0300

    Update IMessageBroker.cs

--- Entities/Contracts/IMessageBroker.cs
+++ Entities/Contracts/IMessageBroker.cs
@@ -1,14 +1,7 @@
 namespace Entities.Contracts
 {
-    /// <summary>
-    /// IMessageBroker interface
-    /// </summary>
     public interface IMessageBroker
     {
-        /// <summary>
-        /// SendMessageToQueu contract
-        /// </summary>
-        /// <param name="message"></param>
         void SendMessageToQueu(string message);
     }
 }
