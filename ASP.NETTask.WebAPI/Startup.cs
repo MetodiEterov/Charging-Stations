e00d709c4cfc99202396310f202223c0ddd2bb4e
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:35:00 +0300

    Update Startup.cs

--- ASP.NETTask.WebAPI/Startup.cs
+++ ASP.NETTask.WebAPI/Startup.cs
@@ -1,46 +1,27 @@
+using ExtensionsMethods;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace ASP.NETTask.WebAPI
 {
-    using ExtensionsMethods;
-
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-
-    /// <summary>
-    /// Startup class
-    /// </summary>
     public class Startup
     {
-        /// <summary>
-        /// Startup constructor
-        /// </summary>
-        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        /// <summary>
-        /// Configuration property
-        /// </summary>
         public IConfiguration Configuration { get; }
 
-        /// <summary>
-        /// ConfigureServices method
-        /// </summary>
-        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureConfigureServicesInjuection(Configuration);
         }
 
-        /// <summary>
-        /// Configure method
-        /// </summary>
-        /// <param name="app"></param>
-        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
