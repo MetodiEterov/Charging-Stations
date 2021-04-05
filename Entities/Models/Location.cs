7bb4cb0258e7f4825ac00bed89bce3b3366921d2
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:41:26 +0300

    Update Location.cs

--- Entities/Models/Location.cs
+++ Entities/Models/Location.cs
@@ -1,15 +1,9 @@
+using System.Collections.Generic;
+
 namespace Entities.Models
 {
-    using System.Collections.Generic;
-
-    /// <summary>
-    /// Location class
-    /// </summary>
     public class Location : BaseClass
     {
-        /// <summary>
-        /// ChargePoints property
-        /// </summary>
         public ICollection<ChargePoint> ChargePoints { get; set; }
     }
 }
