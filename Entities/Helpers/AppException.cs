d54ff30f4f1747f6d2c4dea98b6188e97851da2c
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:39:27 +0300

    Update AppException.cs

--- Entities/Helpers/AppException.cs
+++ Entities/Helpers/AppException.cs
@@ -1,29 +1,14 @@
-﻿namespace Entities.Helpers
-{
-    using System;
-    using System.Globalization;
+using System;
+using System.Globalization;
 
-    /// <summary>
-    /// AppException class
-    /// </summary>
+namespace Entities.Helpers
+{
     public class AppException : Exception
     {
-        /// <summary>
-        /// AppException constructor
-        /// </summary>
         public AppException() : base() { }
 
-        /// <summary>
-        /// AppException constructor
-        /// </summary>
-        /// <param name="message"></param>
         public AppException(string message) : base(message) { }
 
-        /// <summary>
-        /// AppException constructor
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="args"></param>
         public AppException(string message, params object[] args)
             : base(string.Format(CultureInfo.CurrentCulture, message, args))
         {
