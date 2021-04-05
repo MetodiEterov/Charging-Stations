40d41a0778330497770cf1f309b294bec47ad71a
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:33:29 +0300

    Update ExtensionConfigureServices.cs

--- ASP.NETTask.WebAPI/ExtensionsMethods/ExtensionConfigureServices.cs
+++ ASP.NETTask.WebAPI/ExtensionsMethods/ExtensionConfigureServices.cs
@@ -1,29 +1,21 @@
-﻿namespace ASP.NETTask.WebAPI.ExtensionsMethods
+using System;
+
+using AutoMapper;
+
+using Entities.DbContext;
+using Entities.Models;
+using Entities.Contracts;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+using Repository.Repositories;
+
+namespace ASP.NETTask.WebAPI.ExtensionsMethods
 {
-    using System;
-
-    using AutoMapper;
-
-    using Entities.DbContext;
-    using Entities.Models;
-    using Entities.Contracts;
-
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Configuration;
-
-    using Repository.Repositories;
-
-    /// <summary>
-    /// ExtensionConfigureServices class
-    /// </summary>
     public static class ExtensionConfigureServices
     {
-        /// <summary>
-        /// ConfigureConfigureServicesInjuection method
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="configuration"></param>
         public static void ConfigureConfigureServicesInjuection(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ChargingStationsDbContext>(options =>
