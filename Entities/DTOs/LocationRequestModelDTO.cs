4ab95f439537e84a3e57d57df440e26ffa7b0e20
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:37:48 +0300

    Update LocationRequestModelDTO.cs

--- Entities/DTOs/LocationRequestModelDTO.cs
+++ Entities/DTOs/LocationRequestModelDTO.cs
@@ -1,50 +1,23 @@
+using System;
+
 namespace Entities.DTOs
 {
-    using System;
-
-    /// <summary>
-    /// LocationRequestModelDto class
-    /// </summary>
     public class LocationRequestModelDto
     {
-        /// <summary>
-        /// LocationId property
-        /// </summary>
         public string LocationId { get; set; }
 
-        /// <summary>
-        /// Type property
-        /// </summary>
         public string Type { get; set; }
 
-        /// <summary>
-        /// Name property
-        /// </summary>
         public string Name { get; set; }
 
-        /// <summary>
-        /// Address property
-        /// </summary>
         public string Address { get; set; }
 
-        /// <summary>
-        /// City property
-        /// </summary>
         public string City { get; set; }
 
-        /// <summary>
-        /// PostalCode property
-        /// </summary>
         public string PostalCode { get; set; }
 
-        /// <summary>
-        /// Country property
-        /// </summary>
         public string Country { get; set; }
 
-        /// <summary>
-        /// LastUpdated property
-        /// </summary>
         public DateTime LastUpdated { get; set; }
     }
 }
