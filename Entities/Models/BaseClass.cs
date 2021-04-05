cec94634c48497b6e9aa6f82fab19c55ca9dde35
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:40:12 +0300

    Update BaseClass.cs

--- Entities/Models/BaseClass.cs
+++ Entities/Models/BaseClass.cs
@@ -1,64 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.Models
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-
-    /// <summary>
-    /// BaseClass class
-    /// </summary>
     public abstract class BaseClass
     {
-        /// <summary>
-        /// LocationId property
-        /// </summary>
         [Required]
         [StringLength(39)]
         public string LocationId { get; set; }
 
-        /// <summary>
-        /// Type property
-        /// </summary>
         [Required]
         [StringLength(45)]
         public string Type { get; set; }
 
-        /// <summary>
-        /// Name property
-        /// </summary>
         [StringLength(255)]
         public string Name { get; set; }
 
-        /// <summary>
-        /// Address property
-        /// </summary>
         [Required]
         [StringLength(45)]
         public string Address { get; set; }
 
-        /// <summary>
-        /// City property
-        /// </summary>
         [Required]
         [StringLength(45)]
         public string City { get; set; }
 
-        /// <summary>
-        /// PostalCode property
-        /// </summary>
         [Required]
         [StringLength(10)]
         public string PostalCode { get; set; }
 
-        /// <summary>
-        /// Country property
-        /// </summary>
         [Required]
         [StringLength(45)]
         public string Country { get; set; }
-
-        /// <summary>
-        /// LastUpdated property
-        /// </summary>
+ 
         [Required]
         public DateTime LastUpdated { get; set; }
     }
