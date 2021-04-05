bbce28f69d83df7fb259b43cc58f3db1ddbcecd7
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:36:08 +0300

    Update ILoggerManager.cs

--- Entities/Contracts/ILoggerManager.cs
+++ Entities/Contracts/ILoggerManager.cs
@@ -1,32 +1,13 @@
-﻿namespace Entities.Contracts
+namespace Entities.Contracts
 {
-    /// <summary>
-    /// ILoggerManager interface
-    /// </summary>
     public interface ILoggerManager
     {
-        /// <summary>
-        /// LogInfo contract
-        /// </summary>
-        /// <param name="message"></param>
         void LogInfo(string message);
 
-        /// <summary>
-        /// LogWarn contract
-        /// </summary>
-        /// <param name="message"></param>
         void LogWarn(string message);
 
-        /// <summary>
-        /// LogDebug contract
-        /// </summary>
-        /// <param name="message"></param>
         void LogDebug(string message);
 
-        /// <summary>
-        /// LogError contract
-        /// </summary>
-        /// <param name="message"></param>
         void LogError(string message);
     }
 }
