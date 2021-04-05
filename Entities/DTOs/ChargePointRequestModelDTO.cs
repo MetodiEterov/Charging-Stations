a63670233db16dad6cd9e9bd20ad3f8ba7f4573f
MetodiEterov <40655914+MetodiEterov@users.noreply.github.com>
2021-04-05 17:37:11 +0300

    Update ChargePointRequestModelDTO.cs

--- Entities/DTOs/ChargePointRequestModelDTO.cs
+++ Entities/DTOs/ChargePointRequestModelDTO.cs
@@ -1,20 +1,11 @@
+using System.Collections.Generic;
+
 namespace Entities.DTOs
 {
-    using System.Collections.Generic;
-
-    /// <summary>
-    /// ChargePointRequestModelDto class
-    /// </summary>
     public class ChargePointRequestModelDto
     {
-        /// <summary>
-        /// LocationId property
-        /// </summary>
         public string LocationId { get; set; }
 
-        /// <summary>
-        /// ChargePoints property
-        /// </summary>
         public ICollection<ChargePointDto> ChargePoints { get; set; }
     }
 }
